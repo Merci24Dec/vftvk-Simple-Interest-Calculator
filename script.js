5013e6b9dca5e701765fba497fe3d9fc9505bf48
Akash Dubey <akashdubey24122707@gmail.com>
2021-10-16 12:07:52 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	principalElement = document.getElementById("principal");
+    principal = principalElement.value;
+	if(isNaN(principal) || principal.length == 0 || principal < 0) {
+		alert("Please insert a positive number");
+		principalElement.focus();
+		principalElement.classList.add("blueBorder");
+	} else {
+		principalElement.classList.remove("blueBorder");
+		rate = document.getElementById("rate").value;
+		years = document.getElementById("years").value;
+		interest = principal * years * rate / 100;
+		currentDate = new Date();
+		currentDate.setFullYear(currentDate.getFullYear() + parseInt(years));
+		futureYear = currentDate.getFullYear();
+
+		document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>"
+													+ "<br/> at an interest rate of <mark>" + rate + "%</mark>"
+													+ "<br/> You will receive an amount of <mark>" + interest + "</mark>"
+													+ "<br/> in the year <mark>" + futureYear + "</mark>"
+	}
+}
+
+function updateRate() {
+	rate = document.getElementById("rate").value;
+	document.getElementById("rateValue").innerHTML = rate + "%";
 }
         
